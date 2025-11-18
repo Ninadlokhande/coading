a14ae5e499617e0be219dac95f98a418df4d910d
ninadlokhande <lokhandeninad1010@email.com>
2025-11-19 00:22:58 +0530

    reinitializing commit

--- src/area.java
+++ src/area.java
@@ -3,8 +3,46 @@ import java.util.*;
 public class area {
         public static Double findrect (Double w, Double l){
             return w*l;
-
         }
+        public static Double findcir (Double r){
+            return Math.PI*Math.pow(r,2);
+        }
+        public static Double findtri (Double b ,Double h){
+            return 0.5*b*h;
+        }
+        public static Double findsqua(Double l){
+            return Math.pow(l,2);
+        }
+        public static Double findelli(Double a, Double b){
+            return Math.PI*a*b;
+        }
+        public static void squaarea() {
+            Scanner s = new Scanner(System.in);
+            System.out.println(" enter the length of the square");
+            Double length = s.nextDouble();
+
+            System.out.println(" the area of square is:"+ findsqua(length));
+        }
+    public static void triarea(){
+        Scanner s = new Scanner(System.in);
+        System.out.println(" enter the base of the triangle");
+        Double base = s.nextDouble();
+        System.out.println(" enter the height of the rectangle");
+        Double height = s.nextDouble();
+
+        System.out.println("the area of triagnle is;"+findtri(base,height));
+
+    }
+    public static void elliarea(){
+        Scanner s = new Scanner(System.in);
+        System.out.println(" enter the R1 of the ecllipse");
+        Double a = s.nextDouble();
+        System.out.println(" enter the R2 of the ecllipse");
+        Double b = s.nextDouble();
+
+        System.out.println( " the area of ellipce is:"+ findelli(a,b));
+
+    }
         public static void rectarea(){
 
             Scanner s = new Scanner(System.in);
@@ -13,25 +51,59 @@ public class area {
             System.out.println(" enter the length of the rectangle");
             Double length = s.nextDouble();
 
-            System.out.println(findrect(width,length));
-            System.out.println("do you want to calculate area  again(Y/N)");
-            s.nextLine();
-            String c = s.nextLine();
-            if("Y".equals(c) || "y".equals(c)){
-                rectarea();
-            }
-            else{
-                System.out.println("the area calculator is stopped");
-            }
+            System.out.println(" the area of rectangle is:"+ findrect(width,length));
 
+
+        }
+        public static void cirarea() {
+            Scanner s = new Scanner(System.in);
+            System.out.println(" enter the radius of the circle");
+            Double radius = s.nextDouble();
+            System.out.println(" the area of circle is "+ findcir(radius));
 
         }
         public static void main(String[] args){
             Scanner s = new Scanner(System.in);
             System.out.println(" THE AREA CALCULATOR");
-            rectarea();
+            System.out.println(" enter the input among(rect(⏹️), squa(⬜) ,tri(📐),cir(⭕),elli(⚪))");
+            String shape = s.nextLine();
+            String choice = "y";
+            boolean decision = true;
+            while(decision){
+
+            switch (shape){
+                case "rect":
+                   rectarea();
+                    break;
+                case "cir":
+                     cirarea();
+                    break;
+                case "squa":
+                    squaarea();
+                    break;
+                case "elli":
+                     elliarea();
+                    break;
+                case "tri":
+                     triarea();
+                     break;
+                default:
+                    System.out.println(" Invalid input entered"+"  "+"enter the input among(rect,squa,tri,cir,elli)");
+            }
+
+                System.out.println(" do you want to continue");
+                choice = s.nextLine();
+                if(choice.equals("y")|| choice.equals("Y")){
+                    decision = true;
+                    System.out.println(" enter the input among(rect, squa ,tri,cir,elli)");
+                    shape = s.nextLine();
+                }
+                else{
+                    decision =false;
+                }
+
 
         }
 
 
-    }
+    }}
